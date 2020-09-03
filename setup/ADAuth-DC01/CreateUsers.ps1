7f17394abe88f271e7d7116b1985e8f5d030d0ea
Rudolf Kleijwegt <10358044+swerveshot@users.noreply.github.com>
2020-09-03 11:51:45 +0200

    Changed CreateUsers.ps1 to wait for ADWS service
    This change makes sure the Active Directory Web Services are running before creating users.

--- setup/ADAuth-DC01/CreateUsers.ps1
+++ setup/ADAuth-DC01/CreateUsers.ps1
@@ -1,5 +1,11 @@
+Write-Output "***** Creating users in Active Directory"
 Import-Module ActiveDirectory
 $Password = ConvertTo-SecureString "VMware1!" -AsPlainText -Force
+
+# Waiting for the Active Directory Web Services to become available or the AD CMDlets won't work
+(Get-Service -DisplayName "Active Directory Web Services").WaitForStatus('Running')
+Start-Sleep -Seconds 3
+
 New-ADUser -Name "vSphereIAM" -GivenName "vSphereIAM" -SamAccountName "vSphereIAM" -UserPrincipalName "vSphereIAM@initiqlabs.local" -Description "User that is granted permissions to change local ESXi credentials" -PasswordNeverExpires:$true -AccountPassword $Password -Enabled $true
 New-ADUser -Name "vSphereAdmin" -GivenName "vSphereAdmin" -SamAccountName "vSphereAdmin" -UserPrincipalName "vSphereAdmin@initiqlabs.local" -Description "User that is granted permissions to administer vSphere" -PasswordNeverExpires:$true -AccountPassword $Password -Enabled $true
 New-ADGroup -Name "vSphereAdmins" -SamAccountName vSphereAdmins -GroupCategory Security -GroupScope Global -DisplayName "vSphereAdmins" -Description "Users that are granted permissions to administer vSphere"
